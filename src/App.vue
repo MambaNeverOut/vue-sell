5a4724e5ecf371cb4badd5b867d429cba3b61d6c
yaokai <yaokai729@outlook.com>
2019-08-08 18:25:20 +0800

    完成header组件，shopcart组件，goods组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VHeader from "components/v-header/v-header";
+import { getSeller } from "api";
+import Tab from "components/tab/tab";
+import Seller from "components/seller/seller";
+import Goods from "components/goods/goods";
+import Ratings from "components/ratings/ratings";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  computed: {
+    tabs() {
+      return [
+        {
+          label: "商品",
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: "评论",
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: "商家",
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ];
+    }
+  },
   components: {
-    HelloWorld
+    VHeader,
+    Tab
+  },
+  created() {
+    this._getSeller();
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then(seller => {
+        this.seller = seller;
+      });
+    }
   }
-}
+};
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  .tab-wrapper {
+    position: fixed;
+    top: 136px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+}
 </style>
